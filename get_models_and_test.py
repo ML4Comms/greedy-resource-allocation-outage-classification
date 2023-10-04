0d3c8a99751c37abf8350d16572bc4db02af366a
nidhisimmons <dr.nidhisimmons@gmail.com>
2023-10-04 17:49:39 +0100

    Add best of n and refine saved output

--- get_models_and_test.py
+++ get_models_and_test.py
@@ -13,7 +13,11 @@ P_1 = {}
 P_R = {}
 P_best_N = {}
 resources_used = {}
-average_outage_counters = {}
+P_R_average_outage_counters = {}
+best_N_average_outage_counters = {}
+P_1_average_outage_counters = {}
+P_inf_average_outage_counters = {}
+cdf_average_outage_counters = {}
 average_resources_used = {}
 number_of_training_routines_per_model = 10
 out = 10
@@ -39,8 +43,12 @@ for qth in qth_range:
                 for rate_threshold in [0.5]:
                     model_result = f"{model_prefix}_rt-{rate_threshold}_r-{resource}_qth--{qth}_lstm-{lstm_size}_out-{out}_phase-{phase_shift}"
                     model_name = model_prefix
-                    average_outage_counters[model_result] = 0
+                    P_R_average_outage_counters[model_result] = 0
                     average_resources_used[model_result] = 0
+                    best_N_average_outage_counters[model_result] = 0
+                    P_inf_average_outage_counters[model_result] = 0
+                    P_1_average_outage_counters[model_result] = 0
+                    cdf_average_outage_counters[model_result] = 0
                     
                     
                     for _ in range(number_of_training_routines_per_model):
@@ -76,8 +84,6 @@ for qth in qth_range:
                                                                         lstm_units=lstm_size,
                                                                         qth=qth)
 
-                        lowest_y_pred = float('inf')  # Set to infinity initially so any value will be lower
-                        best_of_N_in_outage = False
 
                         for _ in range(number_of_tests):
                             X, y_label = training_generator.__getitem__(0)
@@ -86,39 +92,45 @@ for qth in qth_range:
                             resource_used = 0
                             
                             should_count = True
+                            lowest_y_pred = float('inf')  # Set to infinity initially so any value will be lower
+                            best_of_N_in_outage = False
+                            idx_of_best = 0
                             # Loop over the pre-calibrated outputs
                             for idx, y_pred in enumerate(Y_pred):
                                
+                               # P1 calculation
                                 P_1_counter += 1
                                 P_1[model_result] += float(y_label[idx][0])
                                     
-                                
+                                # P_inf and CDF calculation
                                 cdf_counter += 1
                                 if y_pred[0] <= qth:
                                     cdf[model_result] += 1.0
-
-                                if y_pred[0] <= qth:
+                                    
                                     P_inf_counter += 1
                                     P_inf[model_result] += float(y_label[idx][0])
                                 
-                                resource_used = idx
-                                if should_count:
-                                    if idx == resource-1:
-                                        if y_label[idx][0] >= 0.5:
-                                            P_R[model_result] += 1.0
-                                            should_count = False
-                                    elif y_pred[0] <= qth:
-                                        if y_label[idx][0] >= 0.5:
-                                            P_R[model_result] += 1.0
-                                            should_count = False
-
+                                # P_best_N calculation
                                 if y_pred[0] < lowest_y_pred:
                                     lowest_y_pred = y_pred[0]
-
+                                    idx_of_best = idx
                                     if y_label[idx][0] >= 0.5:
                                         best_of_N_in_outage = True
                                     else:
                                         best_of_N_in_outage = False
+
+                                # P_R
+                                if should_count:
+                                    # If last resource is reached, use resource that gave lowest output
+                                    if idx == resource-1:
+                                        resource_used = idx_of_best
+                                        P_R[model_result] += int(best_of_N_in_outage)
+                                    elif y_pred[0] <= qth:
+                                        resource_used = idx
+                                        should_count = False
+                                        if y_label[idx][0] >= 0.5:
+                                            P_R[model_result] += 1.0
+
 
                             if best_of_N_in_outage:
                                 P_best_N[model_result] += 1.0
@@ -127,13 +139,22 @@ for qth in qth_range:
                             # print(f"{model_result}, "f"Test {_}:", f"Used sub-band number {resource_used}")
                        
                         P_best_N[model_result] = P_best_N[model_result] / number_of_tests
+                        best_N_average_outage_counters[model_result]+=P_best_N[model_result]
+
                         P_R[model_result] = P_R[model_result] / number_of_tests
-                        average_outage_counters[model_result]+=P_R[model_result]
+                        P_R_average_outage_counters[model_result]+=P_R[model_result]
+
                         resources_used[model_result] = resources_used[model_result] / number_of_tests
                         average_resources_used[model_result]+=resources_used[model_result]
+
                         P_1[model_result] = P_1[model_result] / P_1_counter
+                        P_1_average_outage_counters[model_result] += P_1[model_result]
+
                         P_inf[model_result] = (0 if P_inf_counter ==0 else P_inf[model_result] / P_inf_counter)
+                        P_inf_average_outage_counters[model_result] += P_inf[model_result]
+
                         cdf[model_result] = cdf[model_result] / cdf_counter
+                        cdf_average_outage_counters[model_result]+=cdf[model_result]
 
                         with open(f'simulation_results_{resource}_{model_prefix}_microqth.txt', 'a') as convert_file:
                             convert_file.write("\nData configuration:\n")
@@ -160,11 +181,23 @@ for qth in qth_range:
                             convert_file.write("\n=====================================\n")
 
                                                        
-                    average_outage_counters[model_result] = average_outage_counters[model_result] / number_of_training_routines_per_model
+                    P_R_average_outage_counters[model_result] = P_R_average_outage_counters[model_result] / number_of_training_routines_per_model
+                    best_N_average_outage_counters[model_result] = best_N_average_outage_counters[model_result] / number_of_training_routines_per_model
+                    P_inf_average_outage_counters[model_result] = P_inf_average_outage_counters[model_result] / number_of_training_routines_per_model
                     average_resources_used[model_result] = average_resources_used[model_result] / number_of_training_routines_per_model
-                    with open(f'final_results_{resource}_{model_prefix}.txt', 'w') as convert_file:
-                        convert_file.write("\n\Outage probability:\n")
-                        convert_file.write(json.dumps(average_outage_counters, indent=4))
+                    P_1_average_outage_counters[model_result] = P_1_average_outage_counters[model_result] / number_of_training_routines_per_model
+                    cdf_average_outage_counters[model_result] = cdf_average_outage_counters[model_result] / number_of_training_routines_per_model
+                    with open(f'final_results.txt', 'w') as convert_file:
+                        convert_file.write("P_1:\n")
+                        convert_file.write(json.dumps(P_1_average_outage_counters, indent=4))
+                        convert_file.write("\n\nP_R:\n")
+                        convert_file.write(json.dumps(P_R_average_outage_counters, indent=4))
+                        convert_file.write("\n\nP_Best_N:\n")
+                        convert_file.write(json.dumps(best_N_average_outage_counters, indent=4))
+                        convert_file.write("\n\nP_inf:\n")
+                        convert_file.write(json.dumps(P_inf_average_outage_counters, indent=4))
+                        convert_file.write("\n\nCDF:\n")
+                        convert_file.write(json.dumps(cdf_average_outage_counters, indent=4))
                         convert_file.write("\n\nSub-bands:\n")
                         convert_file.write(json.dumps(average_resources_used, indent=4))
                         convert_file.write("\n\n=====================================\n")
